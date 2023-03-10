acaab1917bd0309a613c575e33575120adbe4d65
ALDIRON-lols <80191450+ALDIRON-lols@users.noreply.github.com>
2023-03-10 18:12:46 +0500

    menu for mobile screen build and attached with burger menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeEelems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function(){
     console.log('console hamburger.');
 
     if(header.classList.contains('open')){//closing
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        body.classList.remove('noScroll');
+        fadeEelems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })        
     }
     else{//opening
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        header.classList.add('open');   
+        body.classList.add('noScroll');
+        fadeEelems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })        
     }
 })
